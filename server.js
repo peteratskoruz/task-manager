ab90ebeff6f37d536af42b067f68db9524ae75cd
puritan joseph raja <peterjoeeraj234@gmail.com>
2019-02-26 19:35:52 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
-const app = express();
+// Heroku automagically gives us SSL
+// Lets write some middleware to redirect us
+let env = process.env.NODE_ENV || 'development';
+
+let forceSSL = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+if (env === 'production') {
+  app.use(forceSSL);
+}
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static('./dist/task-manager'));
 
 app.get('/*', function(req,res) {
-
-    res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+    
+res.sendFile(path.join(__dirname,'/dist/task-manager/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
